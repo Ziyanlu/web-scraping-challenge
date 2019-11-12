e1564c3831876b6a4d3ff273dbcf414d43527280
Ziyan <ziyan@northwestern.edu>
2019-11-11 21:35:06 -0600

    new

--- Missions_to_Mars/app-1.py
+++ Missions_to_Mars/app-1.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 import scrape_mars
+
 app = Flask(__name__)
+
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/MarsDB"
-mongo = PyMongo(app)
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+
+db = client.MarsDB
+collection = db.mars
+
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
+    mars = list(db.collection.find())
+    print(mars)
     return render_template("index.html", mars=mars)
+
+
 @app.route("/scrape")
 def scrape():
-    mars = mongo.db.mars
+    mars = db.collection
     mars_data = scrape_mars.scrape_all()
     mars.update({}, mars_data, upsert=True)
     return "Scraping Successful!"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
